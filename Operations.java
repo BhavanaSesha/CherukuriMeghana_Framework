dc3e1dc666359cfce32e02892d5323e6851d8949
CherukuriMeghana <50169034+CherukuriMeghana@users.noreply.github.com>
2020-03-10 15:43:48 +0530

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -1,35 +1,32 @@
-package listcomp;
 import java.util.ArrayList;
 import java.util.Iterator;
 public class Operations {
  static ArrayList a1 = new ArrayList(10);
  int fetchElements(int index)
  {
-	
-	 return (Integer) a1.get(index);
-	 
+	 return (Integer) a1.get(index); 
  }
  void AddElement(int ele)
  {
 	 a1.add(ele);
 	 System.out.println("Element is successfully added");
-	 Iterator i = a1.iterator();
-	 while(i.hasNext())
+	 Iterator it = a1.iterator();
+	 while(it.hasNext())
 	 {
 		 System.out.println(i.next());
 	 }
 
  }
- void removeElement(int ele)
+ void removeElement(int element)
  {
-	 a1.remove(ele);
+	 a1.remove(element);
  }
  void printElements()
  {
-	 Iterator i = a1.iterator();
+	 Iterator it = a1.iterator();
 	 while(i.hasNext())
 	 {
-		 System.out.println(i.next());
+		 System.out.println(it.next());
 	 }
  }
 }
